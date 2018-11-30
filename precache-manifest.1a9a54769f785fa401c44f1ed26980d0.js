1171238d2f764351c8851b0f16014cf5ded38676
Ashraf Morad <amorad@gmail.com>
2018-11-30 18:28:28 +0400

    Updates

---
+++ precache-manifest.1a9a54769f785fa401c44f1ed26980d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e94dcb6cde08e2b98d9",
+    "url": "/AnalogDigitalClock/static/js/runtime~main.0e94dcb6.js"
+  },
+  {
+    "revision": "56330612f2690b4c5afa",
+    "url": "/AnalogDigitalClock/static/js/main.56330612.chunk.js"
+  },
+  {
+    "revision": "de62de6ce9b08e977a87",
+    "url": "/AnalogDigitalClock/static/js/1.de62de6c.chunk.js"
+  },
+  {
+    "revision": "56330612f2690b4c5afa",
+    "url": "/AnalogDigitalClock/static/css/main.80c7567b.chunk.css"
+  },
+  {
+    "revision": "3725d15ad6bb66a1f95f02c647131a07",
+    "url": "/AnalogDigitalClock/index.html"
+  }
+];
